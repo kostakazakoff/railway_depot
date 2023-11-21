ffa527ab161c5b97b2f0a6f2e3aaf25e06c9a2c6
Kosta Kazakov <kostakazakoff@gmail.com>
2023-11-21 19:17:04 +0200

    tables articles <-> stores on inventories created

--- database/migrations/2023_11_21_164647_create_inventories_table.php
+++ database/migrations/2023_11_21_164647_create_inventories_table.php
@@ -10,9 +10,14 @@ return new class extends Migration
     {
         Schema::create('inventories', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('article_id');
+            $table->unsignedBigInteger('store_id');
             $table->integer('quantity');
             $table->string('position');
             $table->timestamps();
+
+            $table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade');
+            $table->foreign('store_id')->references('id')->on('stores')->onDelete('cascade');
         });
     }
     

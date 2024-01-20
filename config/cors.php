80a11c5ee7a05e165966b2eb00374c8a0a640fee
Kosta Kazakov <kostakazakoff@gmail.com>
2024-01-20 02:30:02 +0200

    419 error issue solved

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
@@ -16,6 +16,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];

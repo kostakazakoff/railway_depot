e4cd175bd52238e87adc0bb415ac63a19c7ec3b3
Kosta Kazakov <kostakazakoff@gmail.com>
2024-02-27 20:23:00 +0200

    User register edited

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 // TODO: Admin, users
@@ -16,6 +17,14 @@ class AuthController extends Controller
 {
     public function register(Request $request): JsonResponse
     {
+        $validator = Validator::make($request->all(), [
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:4'],
+        ]);
+        if($validator->fails()) {
+            return response()->json(['status' => false, 'message' => 'fix errors', 'errors' => $validator->errors()], 500);
+        }
+
         $user = User::create([
             'email' => $request->email,
             'password' => Hash::make($request->password)

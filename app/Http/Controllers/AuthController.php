4fd7b64e727d32af66a596f4dbf708902f332c2b
Kosta Kazakov <kostakazakoff@gmail.com>
2024-12-12 16:58:13 +0200

    cleanup AuthController imports

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,7 +10,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use App\Exceptions\AppException;
 use App\Http\Requests\ChangeForgotenPasswordRequest;
 use App\Http\Requests\ForgotPasswordRequest;
